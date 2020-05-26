8d0cfad77cace3de9e80dd5430f5ffc3baa36115
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-05-25 23:14:17 -0400

    created todo list & last item display component, also included 1st draft of css web app design

---
+++ src/components/LastItemDisplay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../syles/LastItemDisplay.css';
+
+export default class LastItemDisplay extends Component {
+  render() {
+    return (
+      <div id="last-item-display">
+        <h4> last item display </h4>
+      </div>
+    )
+  }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../syles/TodoList.css';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div className="todo">
+        <h3> list </h3>
+      </div>
+    )
+  }
+}
