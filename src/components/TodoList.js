adc3d09dd8187abf43a5a3097842d2456c18c336
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-05-26 07:52:45 -0400

    created  submitform function and binded

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,9 +5,25 @@ export default class TodoList extends Component {
   constructor() {
     super()
     this.state = {
-      savedTodoItems: [ "Make a todo list of all my todo lists", "Make a playlist for every possible occasion", "Weekly recipe"]
+      savedTodoItems: [ "Make a todo list of all my todo lists", "Make a playlist for every possible occasion", "Meals I want to eat this week"]
     }
+    this.submitForm = this.submitForm.bind(this);
   }
+
+  submitForm(event) {
+    event.preventDefault();
+
+    // grab the input that the user typed in
+    const inputtedItem = document.querySelector("#inputted-item");
+
+    // The parameters "state" & "props" are copies of this components state and props 
+    // That way we can manipulate them without touching the real state or real props 
+    this.setState((state, props) => {
+      state.savedTodoItems.push(inputtedItem.value);
+      return {savedTodoItems: state.savedTodoItems};
+    });
+  }
+  
   render() {
     // --------------
     // loop through all todo items in state using map() 
@@ -19,7 +35,7 @@ export default class TodoList extends Component {
     return (
       <div className="todo">
         <form> 
-          <input id="inputted-item" placeholder="Add Item"></input>
+          <input id="inputted-item" placeholder="Add Item" autocomplete="off"></input>
           <button type="submit" hidden> </button>
         </form>        
         {/* here we are rendering the <h5>'s that we made earlier using the variable "listOfItems" */}
