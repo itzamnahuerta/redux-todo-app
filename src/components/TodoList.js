a63865aa5f88d22325b4f5e134f053bfe1b62cb7
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-05-25 23:47:11 -0400

    updated listofitems

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,13 +5,25 @@ export default class TodoList extends Component {
   constructor() {
     super()
     this.state = {
-      savedTodoItems: [ "make a todo list of all my todo lists", "make a playlist for every possible occasion", "weekly recipe"]
+      savedTodoItems: [ "Make a todo list of all my todo lists", "Make a playlist for every possible occasion", "Weekly recipe"]
     }
   }
   render() {
+    // --------------
+    // loop through all todo items in state using map() 
+    // & render a <h5> tag for each todo item
+    // --------------
+    const listOfItems = this.state.savedTodoItems.map((item) => {
+      return <div id="item-wrapper"><p id="item">{item}</p> </div>
+    })
     return (
       <div className="todo">
-        <h3> list </h3>
+        <form> 
+          <input id="inputted-item" placeholder="Add Item"></input>
+          <button type="submit" hidden> </button>
+        </form>        
+        {/* here we are rendering the <h5>'s that we made earlier using the variable "listOfItems" */}
+        {listOfItems}
       </div>
     )
   }
