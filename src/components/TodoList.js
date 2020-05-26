3a2e8d015bfa05f160f274c75c03591a3965318f
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-05-25 23:30:24 -0400

    created state with three default  items for todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import '../syles/TodoList.css';
 
 export default class TodoList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      savedTodoItems: [ "make a todo list of all my todo lists", "make a playlist for every possible occasion", "weekly recipe"]
+    }
+  }
   render() {
     return (
       <div className="todo">
