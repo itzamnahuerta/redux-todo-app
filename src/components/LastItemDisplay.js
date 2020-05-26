aad7c91f6a939704737bd7d5c789b6fcf358af3b
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-05-26 11:58:55 -0400

    prompt to begin redux

--- src/components/LastItemDisplay.js
+++ src/components/LastItemDisplay.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import '../syles/LastItemDisplay.css';
 
+// -------------------------------------------------------------------
+// Since this component doesn't have access to ToDoList.js's state...
+// This component doesn't know what the last item added was
+// Redux can help fix this
+// -------------------------------------------------------------------
+
 export default class LastItemDisplay extends Component {
   render() {
     return (
