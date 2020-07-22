cda8e7e80fdbdf4d7be397f38df9622eec64554b
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-07-22 13:32:09 -0400

    last step

--- src/components/LastItemDisplay.js
+++ src/components/LastItemDisplay.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../syles/LastItemDisplay.css';
+import { connect } from 'react-redux';
 
-// -------------------------------------------------------------------
-// Since this component doesn't have access to ToDoList.js's state...
-// This component doesn't know what the last item added was
-// Redux can help fix this
-// -------------------------------------------------------------------
+// ------------------------------------------------------
+// Since this component doesn't have access to TodoDisplay.js's state...
+// this component doesn't know what the last item added was. 
+// Redux can help fix this .
+// ------------------------------------------------------
 
-export default class LastItemDisplay extends Component {
-  render() {
-    return (
-      <div id="last-item-display">
-        <h4> last item display </h4>
-      </div>
-    )
+const mapStateToProps = (store) => ({
+  todoState: store.todoReducer
+})
+
+class LastItemDisplay extends React.Component {
+  render(){
+    console.log("find me at last item display: ", this.props.todoState.savedToDoItems[this.props.todoState.savedToDoItems.length -1])
+       // from updatedstate of savedtodoitems[-1] element and display here 
+       const lastElementinTodo = this.props.todoState.savedToDoItems[this.props.todoState.savedToDoItems.length -1]
+      return (
+        <div id="last-item-display">
+          The last item added goes in here: {lastElementinTodo}
+        </div>
+    );
   }
 }
+
+export default connect(mapStateToProps,null)(LastItemDisplay);
